34dc63cd5fab5cfe95918f3aa4bf0629ef21f674
Metetekin <metetekin111@gmail.com>
2024-10-03 16:41:50 +0300

    v.1.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+## About
+
+It is predetermined where users can register and log in.
+An API application should be developed in which they can be a member of the topic / subject headings. 
+
+## Installation
+
+`composer install` for laravel packages and dependencies.
+
+Copy `.env.example` to `.env` and change app url, app api url and database info.
+
+`php artisan jwt:secret` for generate jwt authenticate key.
+
+`php artisan migrate --seed` for generate database.
+
+## Postman REST Documentation
+
+https://documenter.getpostman.com/view/13845076/TzRYd5Sh
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+"# jwt-restApi" 
+# laravel-RestApi-jwt
+"# laravel-restApi-JWT" 
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role=Role::create(["name" => "Admin"]);
+        $user=User::create([
+            'email' => 'metetekin111@gmail.com',
+            'password' => 123456,
+            'user_type' =>'standart',
+            'bio_text' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'image_url' => 'https://i.pinimg.com/564x/d5/eb/a5/d5eba595ec93552e08be0e19844824ad.jpg'
+        ]);
+        $user->assignRole($role);
+
+    }
+}
